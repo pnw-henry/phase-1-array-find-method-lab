80a396346da9f7f02f05e3ed272ccba85e0a5e93
Henry Escobar <henrye@gmail.com>
2022-05-25 14:39:58 -0700

    commit tree 4ce8f14d3ac9d1a8ebc5b64350a99e952f8b1dd8

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+//Takes in an array of objects containing a 'year' and a 'result'
+//Uses the find() menthod to iterate through the array
+//If condition set by callback function is met, returns the value for year
+function superbowlWin(record){
+    let win = record.find(superbowl);
+    if (!win){
+        return;
+    }
+    else
+        return win.year;
+}
+
+//Takes in an array element and looks for the proterty result to equal 'W'
+function superbowl(element){
+    if (element.result === 'W'){
+        return element;
+    }
+}
